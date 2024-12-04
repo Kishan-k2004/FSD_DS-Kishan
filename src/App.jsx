902c6b01a46d5a22a0a690837b228dd6390b4b7d
Kishan-k2004 <121995452+Kishan-k2004@users.noreply.github.com>
2024-12-04 15:45:16 +0530

    day8

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () =>{
+  return (
+    <div id="disp">
+    <h1>Name : Kishan Khansali</h1>
+    <h2>Email : kishan.22b1541029@abes.ac.in</h2>
+    <h3>College : ABES Engineering Collage</h3>
+    </div>
+  )
+}
+
+export default App
